4dda3fa2cf0e8320b0c27798345ff05126cdf90e
Felipe Pinto da Motta Quevedo <motta.quevedo@ufrgs.br>
2021-01-07 14:23:08 -0300

    Upadte inciando projeto
    Foi iniciado o projeto

---
+++ HelloWorld2/HelloWorld2.f90
@@ -0,0 +1,26 @@
+!  HelloWorld2.f90 
+!
+!  FUNCTIONS:
+!  HelloWorld2 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: HelloWorld2
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program HelloWorld2
+
+    implicit none
+
+    ! Variables
+
+    ! Body of HelloWorld2
+    print *, 'Hello World'
+    pause
+    
+    end program HelloWorld2
+
